b07b6c66696596c89f449fee5f299d5d9f2f9d3e
F0801 <mrabet.feres@esprit.tn>
2025-04-05 15:59:41 +0100

    new Lock Unlock fonctions added for the payment system we lock the payment money until confirmation

--- transPimServ.js
+++ transPimServ.js
@@ -7,6 +7,8 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT;
+const operatorAccount = process.env.MY_ACCOUNT_ID;
+const operatorKey=process.env.MY_PRIVATE_KEY;
 
 // Middleware
 app.use(express.json());
@@ -27,6 +29,45 @@ app.post('/api/wallet/create', async (req, res) => {
   } catch (error) {
     console.error('Error in /api/wallet/create:', error);
     res.status(500).json({ error: 'Failed to create wallet' });
+  }
+});
+//Lock Amount
+app.post('/api/token/Lock', async (req, res) => {
+    try {
+      const { senderAccountId, senderPrivateKey, amount } = req.body;
+  
+      // Ensure required fields are provided
+      if (!senderAccountId || !senderPrivateKey || !amount ) {
+        return res.status(400).json({ error: 'Missing required fields in request body' });
+      }
+
+      await tokenTransfer(senderAccountId, senderPrivateKey, operatorAccount, amount);
+
+    res.status(200).json({ message: 'Token transfer successful' }); 
+
+  } catch (error) {
+    console.error('Error in /api/token/transfer:', error);
+    res.status(500).json({ error: 'Failed to process token transfer' });
+  }
+});
+
+//Unlock Amount
+app.post('/api/token/transfer', async (req, res) => {
+    try {
+      const { receiverAccountId, amount } = req.body;
+  
+      // Ensure required fields are provided
+      if (!receiverAccountId || !amount ) {
+        return res.status(400).json({ error: 'Missing required fields in request body' });
+      }
+
+      await tokenTransfer(operatorAccount, operatorKey, receiverAccountId, amount);
+
+    res.status(200).json({ message: 'Token transfer successful' }); 
+
+  } catch (error) {
+    console.error('Error in /api/token/transfer:', error);
+    res.status(500).json({ error: 'Failed to process token transfer' });
   }
 });
 
@@ -51,6 +92,6 @@ app.post('/api/token/transfer', async (req, res) => {
 });
 
 // Start server
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server listening on port ${PORT}`);
 });
